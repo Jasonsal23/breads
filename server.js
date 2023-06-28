f35359adccc62a7f3a48eece08c8ac331ec13d7f
Jason <jason@Jasons-MacBook-Air.local>
2023-06-27 18:34:58 -0700

    delete route complete

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride=require('method-override')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -15,6 +16,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 // ROUTES
 app.get('/', (req, res) => {
