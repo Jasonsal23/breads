3e0dad2b18178024a1a516d02aa1637c653453cc
Jason <jason@Jasons-MacBook-Air.local>
2023-07-16 14:26:57 -0700

    changes

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const methodOverride=require('method-override')
 const mongoose = require('mongoose')
 require('dotenv').config()
 mongoose.set("strictQuery", false)
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:true},
-  () => {console.log('connected to mongo: ', process.env.MONGO_URI)})
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:true},)
 
 // CONFIGURATION
 //require('dotenv').config()
